56744955be8eca60d9ecf9c0d988bbfa5528fad0
Callum Hutchinson <callumhutchie@hotmail.co.uk>
2025-12-03 12:12:38 +0000

    Tidied Day 3 part 2 up

--- cpp/src/Day3_part2.cpp
+++ cpp/src/Day3_part2.cpp
@@ -5,12 +5,12 @@
 
 long long totalJoltage = 0;
 
-int lineCount = 15;
 int maxDepth = 12;
-int searchGroupSize = lineCount - maxDepth;
+int searchGroupSize;
 
 int FindBiggest(std::string _bankSubset, int *biggestIndex)
 {
+    PROFILE_ME;
     int biggest = 0;
 
     for (int i = 0; i < searchGroupSize; i++)
@@ -26,11 +26,11 @@ int FindBiggest(std::string _bankSubset, int *biggestIndex)
 
 void SearchGroupMethod(std::string _bank)
 {
-
+    PROFILE_ME;
     int digitCount = 0;
     long long currentRunningTotal = 0;
     int offset = 0;
-    while(digitCount < 12)
+    while (digitCount < 12)
     {
         std::string subset = _bank.substr(offset, searchGroupSize);
 
@@ -46,27 +46,6 @@ void SearchGroupMethod(std::string _bank)
     totalJoltage += currentRunningTotal;
 }
 
-void ProcessBank(std::string _bank)
-{
-    PROFILE_ME;
-    // We need to go through the bank and find the biggest pair
-    int previousBiggest = 0;
-
-    for (int leftDigitIndex = 0; leftDigitIndex < _bank.size() - 1; leftDigitIndex++)
-    {
-        for (int rightDigitIndex = leftDigitIndex + 1; rightDigitIndex < _bank.size(); rightDigitIndex++)
-        {
-            int leftDigit = _bank[leftDigitIndex] - 48;
-            int rightDigit = _bank[rightDigitIndex] - 48;
-            int combined = (leftDigit * 10) + rightDigit;
-            if (combined > previousBiggest)
-                previousBiggest = combined;
-        }
-    }
-
-    totalJoltage += previousBiggest;
-}
-
 int main()
 {
     PROFILE_ME;
@@ -75,8 +54,7 @@ int main()
 
     for (int i = 0; i < inputData.size(); i++)
     {
-        lineCount = inputData[i].size();
-        searchGroupSize = lineCount - maxDepth;
+        searchGroupSize = inputData[i].size() - maxDepth;
         searchGroupSize++;
         SearchGroupMethod(inputData[i]);
     }
